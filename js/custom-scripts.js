b279101fa35e12aea1c1cb4f28f6f9dfb2e0f4d6
bIropka <biropka@gmail.com>
2016-01-26 14:01:44 +0200

    v. 0.2

---
+++ js/custom-scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $('.photo-list li').click(function(){
+        if(!$(this).hasClass('active')) {
+            var curIindex = $('.photo-list li').index($(this));
+            switch (curIindex) {
+                case:
+            }
+        }
+    });
+
+});
